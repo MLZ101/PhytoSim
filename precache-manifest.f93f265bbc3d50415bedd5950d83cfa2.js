0cd051902fb0c2b40e4c7adde0ca4eda8cc1be30
MLZ101 <mhmdhsenmalli123@gmail.com>
2023-10-08 16:37:25 +0300

    Updates

---
+++ precache-manifest.f93f265bbc3d50415bedd5950d83cfa2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9855527cf8f8c938cac20a8526264bc",
+    "url": "/PhytoSim/index.html"
+  },
+  {
+    "revision": "760d7f8ba19fb14b2766",
+    "url": "/PhytoSim/static/css/main.728e7f72.chunk.css"
+  },
+  {
+    "revision": "312b7631898c248af3c9",
+    "url": "/PhytoSim/static/js/2.fb50189b.chunk.js"
+  },
+  {
+    "revision": "760d7f8ba19fb14b2766",
+    "url": "/PhytoSim/static/js/main.dce572a8.chunk.js"
+  },
+  {
+    "revision": "b2a45f602b424c40b977",
+    "url": "/PhytoSim/static/js/runtime~main.4886a0ed.js"
+  },
+  {
+    "revision": "f5ce90a99e87eada8dc34e213d3caf31",
+    "url": "/PhytoSim/static/media/bigFish.f5ce90a9.gif"
+  },
+  {
+    "revision": "cb9ae6667a1ca441dc746e42fccc2343",
+    "url": "/PhytoSim/static/media/coral1.cb9ae666.png"
+  },
+  {
+    "revision": "d140374fd7612ecdea2ec01d5ab98d77",
+    "url": "/PhytoSim/static/media/coral2.d140374f.png"
+  },
+  {
+    "revision": "6e54cd3dc5d58891cbc6741272a284c3",
+    "url": "/PhytoSim/static/media/coral3.6e54cd3d.png"
+  },
+  {
+    "revision": "c893506457bc476c454ceb7c01c78a07",
+    "url": "/PhytoSim/static/media/coral4.c8935064.png"
+  },
+  {
+    "revision": "4d0dda6a4e764d986cc6e351a53f32dd",
+    "url": "/PhytoSim/static/media/ocean.4d0dda6a.mp3"
+  },
+  {
+    "revision": "df7a4afd2acf1b4b2b49a3d8dfa0e903",
+    "url": "/PhytoSim/static/media/phyto1.df7a4afd.png"
+  },
+  {
+    "revision": "5d1fcc801850baf62e20244d79f158cb",
+    "url": "/PhytoSim/static/media/phyto3.5d1fcc80.png"
+  },
+  {
+    "revision": "0b58d6b493fb387a00e2b9a444d6aff9",
+    "url": "/PhytoSim/static/media/phyto5.0b58d6b4.png"
+  },
+  {
+    "revision": "85584fcdd998538d957672e32b81c1e5",
+    "url": "/PhytoSim/static/media/phyto6.85584fcd.png"
+  },
+  {
+    "revision": "634c036f25ed9a93f8712044de7acf7f",
+    "url": "/PhytoSim/static/media/phyto7.634c036f.png"
+  },
+  {
+    "revision": "9577933c61e9a09387ce3710e93acc28",
+    "url": "/PhytoSim/static/media/s1.9577933c.mp3"
+  },
+  {
+    "revision": "9c14bcbd8f5cb89d5517ebcb7d77d773",
+    "url": "/PhytoSim/static/media/s2.9c14bcbd.mp3"
+  },
+  {
+    "revision": "1011423888c805d6684dbc0bc0e3fc00",
+    "url": "/PhytoSim/static/media/smallfish.10114238.gif"
+  },
+  {
+    "revision": "c37d3c2fcd071de9103f2c17345a713a",
+    "url": "/PhytoSim/static/media/zooplankton.c37d3c2f.png"
+  }
+]);
